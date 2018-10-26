ccf6430fbf1b5e35f5838174aed38ef63f147e0c
freshma <reshma.f@idexcel.com>
2018-10-26 11:55:01 +0530

    commit

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -19,11 +19,8 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -33,139 +30,98 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-
 import com.relevantcodes.extentreports.LogStatus;
 
 
 public abstract class BaseTest {
- public WebDriver driver=null;
- DesiredCapabilities capability=new DesiredCapabilities();
- ChromeOptions options=null;
- static Logger log = Logger.getLogger(BaseTest.class.getName());
- Map<WebDriver, String> drivers=new HashMap<>();
+    public WebDriver driver=null;
+    DesiredCapabilities capability=new DesiredCapabilities();
+    ChromeOptions options=null;
 
- 
- @Parameters({"browserName","localBrowserPath","selGrid","nodeURL","url"})
- @BeforeMethod(alwaysRun=true)
- public void beforeMethod(@Optional ("chrome") String browserName,@Optional ("D:\\AWS\\Project\\ReshmaBamboo-master\\ReshmaBamboo-master\\src\\test\\resources\\chromedriver") String localBrowserPath,
-   @Optional("false") Boolean selGrid,@Optional ("http://172.16.11.44:5566/wd/hub") String nodeURL,@Optional ("https://www.google.com/") String url,
-   Method method
- ) throws IOException {
-  
-  if(selGrid) {
-     /* System.out.println("selGrid is "+selGrid);
-      //String log4jConfPath = "log4j.properties";
-      // PropertyConfigurator.configure(log4jConfPath);
-    //  DesiredCapabilities capability = DesiredCapabilities.chrome();
-      ChromeOptions capability =new ChromeOptions();
-      capability.setCapability("","");
-     // capability.setBrowserName(browserName);
-     // capability.setPlatform(Platform.extractFromSysProperty(os));
-      this.driver = new RemoteWebDriver(new URL(nodeURL), capability);*/
+    Map<WebDriver, String> drivers=new HashMap<>();
 
 
-      System.out.println("Rakesh testing grid");
-      URL hub = new URL("http://172.16.5.89:4444/wd/hub");
-      DesiredCapabilities browser = DesiredCapabilities.chrome() ;
-      driver = new RemoteWebDriver(hub,browser);//http://172.16.5.89:4444/wd/hub
-  }
-  else {
-      String thread = Thread.currentThread().getName();
-      switch (browserName.toUpperCase()) {
-          case "CHROME":
-
-              final String dir_localBrowserPath = System.getProperty("user.dir") + "/src/test/resources/" + "chromedriver";
-              //   final String dir_localBrowserPath = System.getProperty("user.dir")+"\\src\\test\\resources\\"+localBrowserPath;
-//
-              ChromeDriverService service = new ChromeDriverService.Builder()
-                      .usingDriverExecutable(new File(dir_localBrowserPath))
-                      .usingAnyFreePort()
-                      .build();
-              service.start();
-              /*System.setProperty("webdriver.chrome.driver", dir_localBrowserPath);
-              ChromeOptions options = new ChromeOptions();
-              options.addArguments("port=5566");
-              options.addArguments("headless");
-              options.addArguments("--disable-gpu");
-              options.addArguments("disable-infobars");
-              options.addArguments("--disable-extensions");
-              options.addArguments("window-size=1200x600");
-              options.addArguments("--no-sandbox");
-              WebDriver webDriver = new ChromeDriver(options);*/
-             /* capability = DesiredCapabilities.chrome();
-              options = new ChromeOptions();
-              options.addArguments("Web Test", "start-maximizing", "no-default-browser-check");
-              capability.setCapability(ChromeOptions.CAPABILITY, options);
-              capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-              //  capability.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, true);
-              capability.setPlatform(Platform.ANY);*/
-              if (selGrid) {
-                  driver = new RemoteWebDriver(new URL(nodeURL), capability);
-                  log.info("execution started in the CHROME browser");
-              } else {
-                  driver = new ChromeDriver();
-                  log.info("execution started in the CHROME browser");
-              }
+    @Parameters({"browserName","localBrowserPath","selGrid","nodeURL","url"})
+    @BeforeMethod(alwaysRun=true)
+    public void beforeMethod(@Optional ("chrome") String browserName,@Optional ("D:\\chromedriver.com") String localBrowserPath,
+                             @Optional("false") Boolean selGrid,@Optional ("http://localhost::4444") String nodeURL,@Optional ("https://staging.cyncsoftware.com") String url,
+                             Method method
+    ) throws MalformedURLException {
 
 
-              drivers.put(driver, thread);
-              break;
-          case "FIREFOX":
-              String Xport = System.getProperty("lmportal.xvfb.id", ":1");
-
-              final File firefoxPath = new File(System.getProperty("lmportal.deploy.firefox.path", "/usr/bin/firefox"));
-
-              FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
-
-             /* GeckoDriverService service = new GeckoDriverService.Builder()
-
-              firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
-*/
+        String log4jConfPath = "log4j.properties";
+        PropertyConfigurator.configure(log4jConfPath);
 
 
-              //Below is working code for windows commented by Rakesh for R&D
-             /* System.setProperty("webdriver.gecko.driver", localBrowserPath);
-              capability = DesiredCapabilities.firefox();
-              capability.setCapability("marionette", true);
-              capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-              capability.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, true);
-              if (selGrid) {
-                  driver = new RemoteWebDriver(new URL(nodeURL), capability);
-              } else {
-                  driver = new FirefoxDriver();
-              }
-              drivers.put(driver, thread);*/
-              break;
-          case "IE":
-              System.setProperty("webdriver.ie.driver", localBrowserPath);
-              capability = DesiredCapabilities.internetExplorer();
-              capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-              capability.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, true);
-              if (selGrid) {
-                  driver = new RemoteWebDriver(new URL(nodeURL), capability);
-              } else {
-                  driver = new InternetExplorerDriver();
-              }
-              drivers.put(driver, thread);
-              break;
-      }
-  }
-  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-  
-  log.info("maximizing the Browser");
-  driver.manage().window().maximize();  
- 
-  log.info("Passing the APP URL");
-  driver.get(url);
-  System.out.println(url);
-  ExtentTestManager.startTest(""+method.getName());
-  ExtentTestManager.getTest().log(LogStatus.INFO, "Open Browser and navigate to " + url, "Browser Name: " + browserName);
- }
- 
- 
- @AfterMethod(alwaysRun=true)
- public void afterMethod(ITestResult result,Method method){
-  //int testStatus;
+        String thread=Thread.currentThread().getName();
+        switch(browserName.toUpperCase()){
+            case "CHROME":
+                // /home/leo/Downloads/
+                final String dir_localBrowserPath = System.getProperty("user.dir")+"/src/test/resources/"+localBrowserPath;
+
+                System.setProperty("webdriver.chrome.driver", "D:\\AWS\\Project\\ReshmaBamboo-master\\ReshmaBamboo-master\\src\\test\\resources\\chromedriver");
+
+                capability=DesiredCapabilities.chrome();
+                options=new ChromeOptions();
+                options.addArguments("--headless", "--disable-gpu", "--window-size=1280,1024","--ignore-certificate-errors");
+                capability.setCapability(ChromeOptions.CAPABILITY, options);
+                capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+                //  capability.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, true);
+                capability.setPlatform(Platform.ANY);
+                if(selGrid){
+                    driver=new RemoteWebDriver(new URL(nodeURL),capability);
+                    //log.info("execution started in the CHROME browser");
+                }else{
+                    driver=new ChromeDriver();
+                    //log.info("execution started in the CHROME browser");
+                }
+
+
+                drivers.put(driver, thread);
+                break;
+            case "FIREFOX":
+
+                System.setProperty("webdriver.gecko.driver", "home/likewise-open/IDEXCEL/sankush/cync/Cync_Automation_Project/src/test/resources/geckodriver");
+                capability=DesiredCapabilities.firefox();
+                capability.setCapability("marionette", true);
+                capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+                capability.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, true);
+                if(selGrid){
+                    driver=new RemoteWebDriver(new URL(nodeURL),capability);
+                }else{
+                    driver=new FirefoxDriver();
+                }
+                drivers.put(driver, thread);
+                break;
+            case "IE":
+                System.setProperty("webdriver.ie.driver", localBrowserPath);
+                capability=DesiredCapabilities.internetExplorer();
+                capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+                capability.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, true);
+                if(selGrid){
+                    driver=new RemoteWebDriver(new URL(nodeURL),capability);
+                }else{
+                    driver=new InternetExplorerDriver();
+                }
+                drivers.put(driver, thread);
+                break;
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // log.info("maximizing the Browser");
+        driver.manage().window().maximize();
+
+        //log.info("Passing the APP URL");
+        driver.get(url);
+        System.out.println(url);
+        ExtentTestManager.startTest(""+method.getName());
+        ExtentTestManager.getTest().log(LogStatus.INFO, "Open Browser and navigate to " + url, "Browser Name: " + browserName);
+    }
+
+
+    @AfterMethod(alwaysRun=true)
+    public void afterMethod(ITestResult result,Method method){
+        //int testStatus;
         String fileName = "FAIL  - Error Message Generated on Details Reports";
         String failMsg = "";
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
@@ -182,7 +138,7 @@ public abstract class BaseTest {
             fileName = System.getProperty("user.dir") + "\\Reports\\failure_screenshots\\" + dateEndInString + ".png";
             takeScreenShot(this.driver, fileName);
             ExtentTestManager.getTest().log(LogStatus.FAIL, "Error Screenshort" + ExtentTestManager.getTest().addScreenCapture(fileName));
-          //ExtentTestManager.getTest().log(LogStatus.FAIL, "Error Screenshort" + ExtentTestManager.getTest().addBase64ScreenShot(fileName));
+            //ExtentTestManager.getTest().log(LogStatus.FAIL, "Error Screenshort" + ExtentTestManager.getTest().addBase64ScreenShot(fileName));
             ExtentTestManager.getTest().log(LogStatus.FAIL, result.getThrowable());
             ExtentTestManager.getTest().log(LogStatus.FAIL, "Test Failed");
             testStatus = 2;
@@ -192,26 +148,34 @@ public abstract class BaseTest {
         } else {
             ExtentTestManager.getTest().log(LogStatus.PASS, "Test passed");
             testStatus = 1;
-        }     
+        }
         ExtentManager.getReporter().endTest(ExtentTestManager.getTest());
         ExtentManager.getReporter().flush();
+        // driver.quit();
+        //ExtentTestManager.getTest().log(LogStatus.INFO, "Browser Closed");
+
+        driver.close();
         driver.quit();
         ExtentTestManager.getTest().log(LogStatus.INFO, "Browser Closed");
-  
- }
- 
- public static void takeScreenShot(WebDriver driver, String filePath){
-  TakesScreenshot scrShot = (TakesScreenshot)driver;
+        try {
+            Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void takeScreenShot(WebDriver driver, String filePath){
+        TakesScreenshot scrShot = (TakesScreenshot)driver;
         File srcFile = (File)scrShot.getScreenshotAs(OutputType.FILE);
         File destFile = new File(filePath);
         try {
-   FileUtils.copyFile(srcFile, destFile);
-  } catch (IOException e) {
-   // TODO Auto-generated catch block
-   e.printStackTrace();
-  }
-  
- }
+            FileUtils.copyFile(srcFile, destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
  /*public static String takeScreenShott111(WebDriver driver,String filePath){
  TakesScreenshot newScreen = (TakesScreenshot) driver;
  String scnShot = newScreen.getScreenshotAs(OutputType.BASE64);
